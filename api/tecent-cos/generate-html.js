e0380f2a9c79f6d35bb808410c5afb9c72cf88b5
HuangYi <123337671@qq.com>
2022-02-25 12:33:33 +0800

    :art: 优化不必要的内容

--- api/tecent-cos/generate-html.js
+++ api/tecent-cos/generate-html.js
@@ -9,7 +9,8 @@ const {
   XMLParser
 } = require("fast-xml-parser");
 
-const compiledFunction = pug.compileFile('src/temple.pug');
+const compiledFunction = pug.compileFile(path.resolve(process.cwd(), 'src/temple.pug'));
+const parser = new XMLParser();
 
 /* 以下是环境变量 */
 var cos = new COS({
@@ -17,140 +18,126 @@ var cos = new COS({
   SecretKey: process.env.SecretKey,
 });
 
-const TEMP_PATH = 'tecent_cos_generate_html/';
-const statusFile = "_temp_tecent_cos_generate_html";
 /* 以上是环境变量 */
 
 export default async function handler(req, res) {
   /* 获取参数 */
   let {
     Region = "Region",
-    Bucket = "Bucket",
-    Ignore = "Ignore"
+      Bucket = "Bucket",
+      Ignore = "Ignore"
   } = req.body;
   /* 转数组 */
-  Ignore = eval(Ignore);
+  if (Ignore && Ignore != "") {
+    try {
+      Ignore = eval(Ignore);
+    } catch (e) {
+      console.log(`Error get params of ignore. Error message:`, e)
+      return res.send("Fail");
+    }
+  }
 
   /* 读取当前是否有任务在经行 */
-  let promiseArr = [];
-  let processing = fs.existsSync(statusFile);
-  let tree = { //用于储存树
+  let promiseArr = []; //用于异步上传文件
+  let contents; //存放所有的文件及信息
+  let tree = { //将contents转为树
     Key: "",
     child: []
   };
 
-  /* 还有任务正在经行 */
-  if (processing) {
-    console.log(`Error start function. It is processing`);
+  /* 开始执行任务 */
+  console.log(`Start Function`);
+  /* 获取 COS 所有文件信息（路径、大小、修改时间） */
+  let data = "";
+  try {
+    const res = await getObject({
+      Bucket: Bucket,
+      Region: Region,
+      Key: '/'
+    });
+    data = res.Body;
+    console.log('Get bucket catalog: ', Bucket);
+  } catch (e) {
+    console.log(`Error get bucket catalog. Error message:`, e)
     return res.send("Fail");
   }
-  /* 没有任务正在经行 */
-  else {
-    fs.writeFileSync(statusFile, "233");
-    console.log(`Start Function`);
-    /* 获取 COS 所有文件信息（路径、大小、修改时间） */
-    var data = "";
-    try {
-      const res = await getObject({
+
+  /* 从上步获得的数据中取出变量 */
+  try {
+    contents = parser.parse(data).ListBucketResult.Contents;
+
+    let current = tree;
+    for (let index in contents) {
+      let content = contents[index];
+      let path = content.Key.split('/');
+      /* 根目录文件 || 根目录文件夹 */
+      if ((path.length == 1) || (path.length == 2 && path[1] == '')) {
+        current = tree;
+      }
+      /* 文件夹 */
+      if (path[path.length - 1] == '') {
+        if (!Ignore.includes(path[path.length - 2])) {
+          current.child.push(content);
+        }
+        content.child = [];
+        content.relPath = path[path.length - 2];
+        content.relName = path[path.length - 2] + '/';
+        current = content;
+      }
+      /* 文件 */
+      else {
+        if (!Ignore.includes(path[path.length - 1])) {
+          current.child.push(content);
+        }
+        content.relPath = path[path.length - 1];
+        content.relName = path[path.length - 1];
+      }
+      /* 处理额外的文件信息，用于显示 */
+      let len = content.relName.length;
+      if (len > 76) {
+        content.showInfo = content.LastModified.padStart(24, " ") + ("" + content.Size).padStart(25, " ");
+      } else {
+        content.showInfo = content.LastModified.padStart(100 - len, " ") + ("" + content.Size).padStart(25, " ");
+      }
+    }
+    console.log(`Process Bucket Infomation`);
+  } catch (e) {
+    console.log(`Error process bucket catalog. Error message:`, e);
+    return res.send("Fail");
+  }
+
+  /* 排序 */
+  sortFile(tree);
+
+  /* 输出 */
+  try {
+    let DirList = [tree];
+    for (let i in contents) {
+      if (IsDir(contents[i])) {
+        DirList.push(contents[i]);
+      }
+    }
+
+    promiseArr = DirList.map(Dir => {
+      const params = {
         Bucket: Bucket,
         Region: Region,
-        Key: '/'
-      });
-      data = res.Body;
-      console.log('Get bucket catalog: ', Bucket);
-    } catch (e) {
-      console.log(`Error getting bucket catalog. Error message:`, e)
-      return res.send("Fail");
-    }
-
-    /* 从上步获得的数据中取出变量 */
-    try {
-      const parser = new XMLParser();
-      const contents = parser.parse(data).ListBucketResult.Contents;
-
-      let current = tree;
-      for (let index in contents) {
-        let content = contents[index];
-        let path = content.Key.split('/');
-        /* 根目录文件 && 根目录文件夹 */
-        if ((path.length == 1) || (path.length == 2 && path[1] == '')) {
-          current = tree;
-        }
-        /* 文件夹 */
-        if (path[path.length - 1] == '') {
-          if (!Ignore.includes(path[path.length - 2])) {
-            current.child.push(content);
-          }
-          content.child = [];
-          content.relPath = path[path.length - 2];
-          current = content;
-        }
-        /* 文件 */
-        else {
-          if (!Ignore.includes(path[path.length - 1])) {
-            current.child.push(content);
-          }
-          content.relPath = path[path.length - 1];
-        }
-        let len = content.relPath.length;
-        if (len > 76) {
-          content.showInfo = content.LastModified.padStart(24, " ") + ("" + content.Size).padStart(25, " ");
-        } else {
-          content.showInfo = content.LastModified.padStart(100 - len, " ") + ("" + content.Size).padStart(25, " ");
-        }
+        Key: Dir.Key + 'index.html',
+        StorageClass: 'STANDARD',
+        Body: compiledFunction(Dir), // 上传文件对象
       }
-      console.log(`Process Bucket Infomation`);
-    } catch (e) {
-      console.log(`Error process bucket catalog. Error message:`, e);
-      return res.send("Fail");
-    }
+      return putObject(params);
+    })
 
-    /* 排序 */
-    sortFile(tree);
-
-    /* 输出 */
-    try {
-      generateHtml(tree);
-      console.log(`Generate Html successfully`);
-    } catch (e) {
-      console.log(`Error generateHtml. Error message:`, e);
-      return res.send("Fail");
-    }
-    /* 上传 */
-    try {
-      const files = traversal(TEMP_PATH);
-      promiseArr = files.map(file => {
-        const params = {
-          Bucket: Bucket,
-          Region: Region,
-          Key: file.substr(TEMP_PATH.length),
-          StorageClass: 'STANDARD',
-          Body: fs.createReadStream(file), // 上传文件对象
-        }
-        return putObject(params);
-      })
-      await Promise.all(promiseArr);
-      console.log(`Put files successfully`);
-    } catch (e) {
-      console.log('Error update files. Error message:', e);
-      return res.send("Fail");
-    }
-
-    /* 删除暂存文件 */
-    try {
-      delDir(TEMP_PATH);
-      fs.unlinkSync(statusFile);
-      console.log(`Delete temp files successfully`);
-    } catch (e) {
-      console.log('Update file error. Error message:', e);
-      return res.send("Fail");
-    }
-
-
-
-    /* 响应 */
-    return res.send("Success");
+    await Promise.all(promiseArr);
+    console.log(`Generate and put files successfully`);
+  } catch (e) {
+    console.log(`Error generate or put. Error message:`, e);
+    return res.send("Fail");
   }
+
+  /* 响应 */
+  return res.send("Success");
 }
 
 
@@ -168,23 +155,6 @@ function sortFile(content) {
     if (IsDir(b) && !IsDir(a)) return 1;
     return a - b;
   })
-}
-
-//生成 HTML
-function generateHtml(content) {
-  /* 递归 */
-  if (!fs.existsSync(TEMP_PATH + content.Key)) {
-    fs.mkdirSync(TEMP_PATH + content.Key);
-  }
-
-  /* 生成 */
-  for (let i in content.child) {
-    if (IsDir(content.child[i])) {
-      generateHtml(content.child[i]);
-    }
-  }
-
-  fs.writeFileSync(TEMP_PATH + path.dirname(content.Key + '9cats') + '/index.html', compiledFunction(content));
 }
 
 //判断 content 是否是文件夹
@@ -215,37 +185,4 @@ function putObject(params) {
       }
     })
   })
-}
-
-
-function delDir(dir) {
-  if (fs.existsSync(dir)) {
-    let files = fs.readdirSync(dir);
-    for (let v of files) {
-      let newpath = dir + "/" + v;
-      let stats = fs.statSync(newpath);
-      if (stats.isFile()) {
-        fs.unlinkSync(newpath);
-      } else {
-        delDir(newpath);
-      }
-    }
-    fs.rmdirSync(dir);
-  }
-}
-
-
-function traversal(dir) {
-  let results = []
-  const list = fs.readdirSync(dir)
-  list.forEach(function (file) {
-    file = dir + '/' + file
-    const stat = fs.statSync(file)
-    if (stat && stat.isDirectory()) {
-      results = results.concat(traversal(file))
-    } else {
-      results.push(file)
-    }
-  })
-  return results
 }
